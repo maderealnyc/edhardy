6e76629887a6b2c4860934f84fa7ad06498ca775
Agustin Feliz <agustin.feliz@gmail.com>
2014-03-23 02:49:43 -0400

    HP Layout 1 Responsiveness Grayscale filter Instafeed JS updates

--- homepage-variables-controller.php
+++ homepage-variables-controller.php
@@ -4,162 +4,84 @@
 <?php // Demo Mode Enabled? ?>
 <?php $demo_mode_enabled = true; ?>
 
-<?php /////// Instagram Feed Testing ///////////////////////////////////////////////////////////////////////////// ?>
-
-<h3>Instagram Feed Testing</h3>
-<!--
-<script type="text/javascript">
-    var feed = new Instafeed({
-        get: 'tagged',
-        tagName: 'OfficialEdHardy',
-        clientId: 'b8bbb9934df3402781645208fcfcdcca'
-    });
-    feed.run();
-</script>
--->
-<script type="text/javascript">
-    var intagramTile2 = new Instafeed({
-        get: 'user',
-        userId: 50320184,
-        accessToken: '50320184.467ede5.a99c8bbf270145c9ad8c2414e8e20bad',
-        target: 'intagram_tile_2',
-        sortBy: 'most-recent',
-        resolution: 'low_resolution',
-        template: '<a href="{{link}}" target="_blank"><img src="{{image}}" width="300" height="300"/></a>',
-        limit: 1
-    });
-    intagramTile2.run();
-</script>
-<div id="intagram_tile_2"></div>
-
-<script type="text/javascript">
-    var userFeed = new Instafeed({
-        get: 'user',
-        userId: 50320184,
-        accessToken: '50320184.467ede5.a99c8bbf270145c9ad8c2414e8e20bad',
-        target: 'instafeed',
-        sortBy: 'most-recent',
-        resolution: 'low_resolution',
-        template: '<a href="{{link}}" target="_blank"><img src="{{image}}" width="300" height="300"/></a>',
-        limit: 4
-    });
-    userFeed.run();
-
-    instagram_images = new array();
-	instagram_images.push(userFeed[0]);
-	document.write("instagram_images[0]" + instagram_images[0].name);
-
-</script>
-<div id="instafeed"></div>
-
 <?php /////// Homepage Layout 1 ///////////////////////////////////////////////////////////////////////////// ?>
 
 <?php if( $page_template_slug == 'page-homepage-layout-1.php' ): ?>
 
-	<?php if( $demo_mode_enabled ): ?><hr/><h4>Homepage Layout 1 Variables &amp; Demos</h4><hr/><?php endif; ?>
-
 	<?php // Homepage Layout 1 Tile 1 (Static Image Tile) ?>
 	<?php $hp1_tile_1_bg_image = get_field('hp1_tile_1_bg_image'); ?>
-	<?php // Homepage Layout 1 Tile 1 (Static Image Tile) Demo ?>
-	<?php if( $demo_mode_enabled ): ?>
-		<p>Homepage Layout 1 Tile 1 (Static Image Tile) Background Image: $hp1_tile_1_bg_image['url'];</p>
-	<?php endif; ?>
 	<?php if( $demo_mode_enabled && !empty($hp1_tile_1_bg_image) ): ?>
-		<img src="<?php echo $hp1_tile_1_bg_image['url']; ?>" alt="<?php echo $hp1_tile_1_bg_image['alt']; ?>" />
+		<div class="threecol grid clearfix" id="tile_1"><img class="grayscale" src="<?php echo $hp1_tile_1_bg_image['url']; ?>" alt="<?php echo $hp1_tile_1_bg_image['alt']; ?>" /></div>
 	<?php endif; ?>
 
 	<?php // Homepage Layout 1 Tile 2 (Instagram Tile) ?>
 	<?php if( $demo_mode_enabled ): ?>
-		<p>Homepage Layout 1 Tile 2 (Instagram Tile) : </p>
+		<div class="threecol grid clearfix" id="tile_2"></div>
 	<?php endif; ?>
 
 	<?php // Homepage Layout 1 Tile 3 (Instagram Tile) ?>
 	<?php if( $demo_mode_enabled ): ?>
-		<p>Homepage Layout 1 Tile 3 (Instagram Tile) : </p>
+		<div class="threecol grid clearfix" id="tile_3"></div>
 	<?php endif; ?>
 
 	<?php // Homepage Layout 1 Tile 4 (Latest News Tile) ?>
 	<?php $hp1_tile_4_bg_image = get_field('latest_news_tile_bg_image'); ?>
-	<?php // Homepage Layout 1 Tile 4 (Latest News Tile) Demo ?>
-	<?php if( $demo_mode_enabled ): ?>
-		<p>Homepage Layout 1 Tile 4 (Latest News Tile) Background Image: $hp1_tile_4_bg_image['url'];</p>
-	<?php endif; ?>
 	<?php if( $demo_mode_enabled && !empty($hp1_tile_4_bg_image) ): ?>
-		<a href="#newsletter"><img src="<?php echo $hp1_tile_4_bg_image['url']; ?>" alt="<?php echo $hp1_tile_4_bg_image['alt']; ?>" /></a>
+		<div class="threecol grid clearfix" id="tile_4"><a href="#newsletter"><img class="grayscale" src="<?php echo $hp1_tile_4_bg_image['url']; ?>" alt="<?php echo $hp1_tile_4_bg_image['alt']; ?>" /></a></div>
 	<?php endif; ?>
 
 	<?php // Homepage Layout 1 Tile 5 (Lookbook Tile) ?>
 	<?php $hp1_tile_5_bg_image = get_field('lookbook_tile_bg_image'); ?>
-	<?php // Homepage Layout 1 Tile 5 (Lookbook Tile) Demo ?>
-	<?php if( $demo_mode_enabled ): ?>
-		<p>Homepage Layout 1 Tile 5 (Lookbook Tile) Background Image: $hp1_tile_5_bg_image['url'];</p>
-	<?php endif; ?>
 	<?php if( $demo_mode_enabled && !empty($hp1_tile_5_bg_image) ): ?>
-		<a href="/lookbook"><img src="<?php echo $hp1_tile_5_bg_image['url']; ?>" alt="<?php echo $hp1_tile_5_bg_image['alt']; ?>" /></a>
+		<div class="threecol grid clearfix" id="tile_5"><a href="/lookbook"><img class="grayscale" src="<?php echo $hp1_tile_5_bg_image['url']; ?>" alt="<?php echo $hp1_tile_5_bg_image['alt']; ?>" /></a></div>
 	<?php endif; ?>
 
 	<?php // Homepage Layout 1 Tile 6 (Instagram Tile) ?>
 	<?php if( $demo_mode_enabled ): ?>
-		<p>Homepage Layout 1 Tile 6 (Instagram Tile) : </p>
+		<div class="threecol grid clearfix" id="tile_6"></div>
 	<?php endif; ?>
 
 	<?php // Homepage Layout 1 Tile 7 (Static Image Tile) ?>
 	<?php $hp1_tile_7_bg_image = get_field('hp1_tile_7_bg_image'); ?>
-	<?php // Homepage Layout 1 Tile 7 (Static Image Tile) Demo ?>
-	<?php if( $demo_mode_enabled ): ?>
-		<p>Homepage Layout 1 Tile 7 (Static Image Tile) Background Image: $hp1_tile_7_bg_image['url'];</p>
-	<?php endif; ?>
 	<?php if( $demo_mode_enabled && !empty($hp1_tile_7_bg_image) ): ?>
-		<img src="<?php echo $hp1_tile_7_bg_image['url']; ?>" alt="<?php echo $hp1_tile_7_bg_image['alt']; ?>" />
+		<div class="threecol grid clearfix" id="tile_7"><img class="grayscale" src="<?php echo $hp1_tile_7_bg_image['url']; ?>" alt="<?php echo $hp1_tile_7_bg_image['alt']; ?>" /></div>
 	<?php endif; ?>
 
 	<?php // Homepage Layout 1 Tile 8 (Twitter Feed Tile) ?>
 	<?php $hp1_tile_8_bg_image = get_field('twitter_feed_tile_bg_image'); ?>
-	<?php // Homepage Layout 1 Tile 8 (Twitter Feed Tile) Demo ?>
-	<?php if( $demo_mode_enabled ): ?>
-		<p>Homepage Layout 1 Tile 8 (Twitter Feed Tile) Background Image: $hp1_tile_8_bg_image['url'];</p>
-	<?php endif; ?>
 	<?php if( $demo_mode_enabled && !empty($hp1_tile_8_bg_image) ): ?>
-		<img src="<?php echo $hp1_tile_8_bg_image['url']; ?>" alt="<?php echo $hp1_tile_8_bg_image['alt']; ?>" />
+		<div class="threecol grid grayscale" id="twitter_feed_tile" style="background-image: url(<?php echo $hp1_tile_8_bg_image['url']; ?>);">Twitter Carousel</div>
 	<?php endif; ?>
 
 	<?php // Homepage Layout 1 Tile 9 (About Tile) ?>
 	<?php $hp1_tile_9_bg_image = get_field('about_tile_bg_image'); ?>
-	<?php // Homepage Layout 1 Tile 9 (About Tile) Demo ?>
-	<?php if( $demo_mode_enabled ): ?>
-		<p>Homepage Layout 1 Tile 9 (About Tile) Background Image: $hp1_tile_9_bg_image['url'];</p>
-	<?php endif; ?>
 	<?php if( $demo_mode_enabled && !empty($hp1_tile_9_bg_image) ): ?>
-		<a href="#about"><img src="<?php echo $hp1_tile_9_bg_image['url']; ?>" alt="<?php echo $hp1_tile_9_bg_image['alt']; ?>" /></a>
+		<div class="threecol grid clearfix" id="about_tile"><a href="#about"><img class="grayscale" src="<?php echo $hp1_tile_9_bg_image['url']; ?>" alt="<?php echo $hp1_tile_9_bg_image['alt']; ?>" /></a></div>
 	<?php endif; ?>
 
 	<?php // Homepage Layout 1 Tile 10 (Static Image Tile) ?>
 	<?php $hp1_tile_10_bg_image = get_field('hp1_tile_10_bg_image'); ?>
-	<?php // Homepage Layout 1 Tile 10 (Static Image Tile) Demo ?>
-	<?php if( $demo_mode_enabled ): ?>
-		<p>Homepage Layout 1 Tile 10 (Static Image Tile) Background Image: $hp1_tile_10_bg_image['url'];</p>
-	<?php endif; ?>
 	<?php if( $demo_mode_enabled && !empty($hp1_tile_10_bg_image) ): ?>
-		<img src="<?php echo $hp1_tile_10_bg_image['url']; ?>" alt="<?php echo $hp1_tile_10_bg_image['alt']; ?>" />
+		<div class="threecol grid clearfix" id="tile_10"><img class="grayscale" src="<?php echo $hp1_tile_10_bg_image['url']; ?>" alt="<?php echo $hp1_tile_10_bg_image['alt']; ?>" /></div>
 	<?php endif; ?>
 
 	<?php // Homepage Layout 1 Tile 11 (Follow Us Tile) ?>
 	<?php $hp1_tile_11_bg_image = get_field('follow_us_tile_hp1_bg_image'); ?>
-	<?php // Homepage Layout 1 Tile 11 (Follow Us Tile) Demo ?>
-	<?php if( $demo_mode_enabled ): ?>
-		<p>Homepage Layout 1 Tile 11 (Follow Us Tile) Background Image: $hp1_tile_11_bg_image['url'];</p>
-		<p><a href="https://www.facebook.com/edhardy" target="_blank">Facebook (https://www.facebook.com/edhardy)</a></p>
-		<p><a href="https://twitter.com/edhardy" target="_blank">Twitter (https://twitter.com/edhardy)</a></p>
-		<p><a href="http://instagram.com/OfficialEdHardy" target="_blank">Instagram (http://instagram.com/OfficialEdHardy)</a></p>
-		<p><a href="http://www.pinterest.com/edhardyofficial/" target="_blank">Pinterest (http://www.pinterest.com/edhardyofficial/)</a></p>
-	<?php endif; ?>
 	<?php if( $demo_mode_enabled && !empty($hp1_tile_11_bg_image) ): ?>
-		<img src="<?php echo $hp1_tile_11_bg_image['url']; ?>" alt="<?php echo $hp1_tile_11_bg_image['alt']; ?>" />
+		<div class="threecol grid grayscale clearfix" id="follow_us_tile" style="background-image: url(<?php echo $hp1_tile_11_bg_image['url']; ?>);">
+			<h2>Follow Us</h2>
+			<ul class="clearfix">
+				<li class="facebook"><a class="image-replacement" href="https://www.facebook.com/edhardy" target="_blank">Facebook (https://www.facebook.com/edhardy)</a></li>
+				<li class="twitter"><a class="image-replacement" href="https://twitter.com/edhardy" target="_blank">Twitter (https://twitter.com/edhardy)</a></li>
+				<li class="instagram"><a class="image-replacement" href="http://instagram.com/OfficialEdHardy" target="_blank">Instagram (http://instagram.com/OfficialEdHardy)</a></li>
+				<li class="pinterest"><a class="image-replacement" href="http://www.pinterest.com/edhardyofficial/" target="_blank">Pinterest (http://www.pinterest.com/edhardyofficial/)</a></li>
+			</ul>
+		</div>
 	<?php endif; ?>
 
 	<?php // Homepage Layout 1 Tile 12 (Instagram Tile) ?>
 	<?php if( $demo_mode_enabled ): ?>
-		<p>Homepage Layout 1 Tile 12 (Instagram Tile) : </p>
+		<div class="threecol grid clearfix" id="tile_12"></div>
 	<?php endif; ?>
 
 <?php endif; ?>
@@ -302,3 +224,70 @@
 	<?php endif; ?>
 
 <?php endif; ?>
+
+<?php /////// Instagram Feed Testing ///////////////////////////////////////////////////////////////////////////// ?>
+
+<script type="text/javascript">
+    var intagramTile2 = new Instafeed({
+        get: 'user',
+        userId: 50320184,
+        accessToken: '50320184.467ede5.a99c8bbf270145c9ad8c2414e8e20bad',
+        target: 'tile_2',
+        sortBy: 'random',
+        resolution: 'low_resolution',
+        template: '<a href="{{link}}" target="_blank"><img class="grayscale" src="{{image}}" alt="" /></a>',
+        limit: 1
+    });
+    intagramTile2.run();
+</script>
+
+<script type="text/javascript">
+    var intagramTile3 = new Instafeed({
+        get: 'user',
+        userId: 50320184,
+        accessToken: '50320184.467ede5.a99c8bbf270145c9ad8c2414e8e20bad',
+        target: 'tile_3',
+        sortBy: 'random',
+        resolution: 'low_resolution',
+        template: '<a href="{{link}}" target="_blank"><img class="grayscale" src="{{image}}" alt="" /></a>',
+	    after: function () {
+	        var tile_3_img = jQuery("#tile_3").find('a');
+	        jQuery(tile_3_img.slice(1, tile_3_img.length)).remove();
+	    }
+    });
+    intagramTile3.run();
+</script>
+
+<script type="text/javascript">
+    var intagramTile6 = new Instafeed({
+        get: 'user',
+        userId: 50320184,
+        accessToken: '50320184.467ede5.a99c8bbf270145c9ad8c2414e8e20bad',
+        target: 'tile_6',
+        sortBy: 'random',
+        resolution: 'low_resolution',
+        template: '<a href="{{link}}" target="_blank"><img class="grayscale" src="{{image}}" alt="" /></a>',
+	    after: function () {
+	        var tile_6_img = jQuery("#tile_6").find('a');
+	        jQuery(tile_6_img.slice(1, tile_6_img.length)).remove();
+	    }
+    });
+    intagramTile6.run();
+</script>
+
+<script type="text/javascript">
+    var intagramTile12 = new Instafeed({
+        get: 'user',
+        userId: 50320184,
+        accessToken: '50320184.467ede5.a99c8bbf270145c9ad8c2414e8e20bad',
+        target: 'tile_12',
+        sortBy: 'random',
+        resolution: 'low_resolution',
+        template: '<a href="{{link}}" target="_blank"><img class="grayscale" src="{{image}}" alt="" /></a>',
+	    after: function () {
+	        var tile_12_img = jQuery("#tile_12").find('a');
+	        jQuery(tile_12_img.slice(1, tile_12_img.length)).remove();
+	    }
+    });
+    intagramTile12.run();
+</script>
--- page-homepage-layout-1.php
+++ page-homepage-layout-1.php
@@ -12,20 +12,7 @@ Template Name: Homepage Layout 1
 
 			<div id="main" class="twelvecol first clearfix" role="main">
 
-				<div class="threecol grid"><img src="<?php echo get_stylesheet_directory_uri() ?>/library/images/home/1/home-1.jpg" alt=""></div>
-				<div class="threecol grid"><img src="<?php echo get_stylesheet_directory_uri() ?>/library/images/home/1/home-2.jpg" alt=""></div>
-				<div class="threecol grid"><img src="<?php echo get_stylesheet_directory_uri() ?>/library/images/home/1/home-3.jpg" alt=""></div>
-				<div class="threecol grid"><img src="<?php echo get_stylesheet_directory_uri() ?>/library/images/home/1/home-4.jpg" alt=""></div>
-
-				<div class="threecol grid"><img src="<?php echo get_stylesheet_directory_uri() ?>/library/images/home/1/home-5.jpg" alt=""></div>
-				<div class="threecol grid"><img src="<?php echo get_stylesheet_directory_uri() ?>/library/images/home/1/home-6.jpg" alt=""></div>
-				<div class="threecol grid"><img src="<?php echo get_stylesheet_directory_uri() ?>/library/images/home/1/home-7.jpg" alt=""></div>
-				<div class="threecol grid"><img src="<?php echo get_stylesheet_directory_uri() ?>/library/images/home/1/home-8.jpg" alt=""></div>
-
-				<div class="threecol grid"><img src="<?php echo get_stylesheet_directory_uri() ?>/library/images/home/1/home-9.jpg" alt=""></div>
-				<div class="threecol grid"><img src="<?php echo get_stylesheet_directory_uri() ?>/library/images/home/1/home-10.jpg" alt=""></div>
-				<div class="threecol grid"><img src="<?php echo get_stylesheet_directory_uri() ?>/library/images/home/1/home-11.jpg" alt=""></div>
-				<div class="threecol grid"><img src="<?php echo get_stylesheet_directory_uri() ?>/library/images/home/1/home-12.jpg" alt=""></div>
+				<?php include('homepage-variables-controller.php'); ?>
 
 				<div class="twelvecol grid clearfix"><img src="<?php echo get_stylesheet_directory_uri() ?>/library/images/home/home-bg-1.jpg" alt=""></div>
 
@@ -38,14 +25,12 @@ Template Name: Homepage Layout 1
 					<p>Don Ed Hardy brought tattoo artistry out of the dark and into the glamorous world of fashion. He quickly became recognized for his incorporation of the Japanese art form and American style. In 2002, Hardy stepped into the fashion space when he licensed his art designs to be made into a clothing line. In two short years the Ed Hardy line became one of the most desired and recognizable brands of its time. Don Ed Hardy continues to be one of the most influential designers and his artwork has stood the test of time in the tattoo, art, and fashion world.</p>
 				</div>
 
-				<div id="fb-root"></div> <script>(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//connect.facebook.net/en_US/all.js#xfbml=1"; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk'));</script>
-				<div class="fb-post" data-href="https://www.facebook.com/photo.php?v=1410782879144911" data-width="466"><div class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/photo.php?v=1410782879144911">Post</a> by <a href="https://www.facebook.com/edhardy">ED HARDY</a>.</div></div>
+				<!-- <div id="fb-root"></div> <script>(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//connect.facebook.net/en_US/all.js#xfbml=1"; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk'));</script>
+				<div class="fb-post" data-href="https://www.facebook.com/photo.php?v=1410782879144911" data-width="466"><div class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/photo.php?v=1410782879144911">Post</a> by <a href="https://www.facebook.com/edhardy">ED HARDY</a>.</div></div> -->
 
 			</div>
 
 		</div>
-
-		<?php include('homepage-variables-controller.php'); ?>
 
 	</div>
 
