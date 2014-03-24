fe9e5a968e261dd29061e57b4efbafef142636a7
Agustin Feliz <agustin.feliz@gmail.com>
2014-03-24 15:31:19 -0400

    Grid tweaks Added text links to tiles Added full width class to page.php

--- homepage-variables-controller.php
+++ homepage-variables-controller.php
@@ -52,13 +52,13 @@
 	<?php // Homepage Layout 1 Tile 4 (Latest News Tile) ?>
 	<?php $hp1_tile_4_bg_image = get_field('latest_news_tile_bg_image'); ?>
 	<?php if( !empty($hp1_tile_4_bg_image) ): ?>
-		<div class="threecol grid clearfix" id="tile_4"><a href="#newsletter"><img class="grayscale" src="<?php echo $hp1_tile_4_bg_image['url']; ?>" alt="<?php echo $hp1_tile_4_bg_image['alt']; ?>" /></a></div>
+		<div class="threecol grid gridbg clearfix" id="tile_4" style="background-image: url(<?php echo $hp1_tile_4_bg_image['url']; ?>);"><a href="#newsletter"><h2>Latest <strong>News</strong></h2></a></div>
 	<?php endif; ?>
 
 	<?php // Homepage Layout 1 Tile 5 (Lookbook Tile) ?>
 	<?php $hp1_tile_5_bg_image = get_field('lookbook_tile_bg_image'); ?>
 	<?php if( !empty($hp1_tile_5_bg_image) ): ?>
-		<div class="threecol grid clearfix" id="tile_5"><a href="/lookbook"><img class="grayscale" src="<?php echo $hp1_tile_5_bg_image['url']; ?>" alt="<?php echo $hp1_tile_5_bg_image['alt']; ?>" /></a></div>
+		<div class="threecol grid gridbg clearfix" id="tile_5" style="background-image: url(<?php echo $hp1_tile_5_bg_image['url']; ?>);"><a href="/lookbook"><h2>2014 <strong>Lookbook</strong></h2></a></div>
 	<?php endif; ?>
 
 	<?php // Homepage Layout 1 Tile 6 (Instagram Tile) ?>
@@ -92,13 +92,13 @@
 	<?php // Homepage Layout 1 Tile 8 (Twitter Feed Tile) ?>
 	<?php $hp1_tile_8_bg_image = get_field('twitter_feed_tile_bg_image'); ?>
 	<?php if( !empty($hp1_tile_8_bg_image) ): ?>
-		<div class="threecol grid grayscale" id="twitter_feed_tile" style="background-image: url(<?php echo $hp1_tile_8_bg_image['url']; ?>);">Twitter Carousel</div>
+		<div class="threecol grid gridbg grayscale" id="twitter_feed_tile" style="background-image: url(<?php echo $hp1_tile_8_bg_image['url']; ?>);">Twitter Carousel</div>
 	<?php endif; ?>
 
 	<?php // Homepage Layout 1 Tile 9 (About Tile) ?>
 	<?php $hp1_tile_9_bg_image = get_field('about_tile_bg_image'); ?>
 	<?php if( !empty($hp1_tile_9_bg_image) ): ?>
-		<div class="threecol grid clearfix" id="about_tile"><a href="#about"><img class="grayscale" src="<?php echo $hp1_tile_9_bg_image['url']; ?>" alt="<?php echo $hp1_tile_9_bg_image['alt']; ?>" /></a></div>
+		<div class="threecol grid gridbg clearfix" id="about_tile" style="background-image: url(<?php echo $hp1_tile_9_bg_image['url']; ?>);"><a href="#about"><h2>About <strong>Ed</strong></h2></a></div>
 	<?php endif; ?>
 
 	<?php // Homepage Layout 1 Tile 10 (Static Image Tile) ?>
@@ -110,7 +110,7 @@
 	<?php // Homepage Layout 1 Tile 11 (Follow Us Tile) ?>
 	<?php $hp1_tile_11_bg_image = get_field('follow_us_tile_hp1_bg_image'); ?>
 	<?php if( !empty($hp1_tile_11_bg_image) ): ?>
-		<div class="threecol grid grayscale clearfix" id="follow_us_tile" style="background-image: url(<?php echo $hp1_tile_11_bg_image['url']; ?>);">
+		<div class="threecol grid gridbg grayscale clearfix" id="follow_us_tile" style="background-image: url(<?php echo $hp1_tile_11_bg_image['url']; ?>);">
 			<h2>Follow Us</h2>
 			<ul class="clearfix">
 				<li class="facebook"><a class="image-replacement" href="https://www.facebook.com/edhardy" target="_blank">Facebook (https://www.facebook.com/edhardy)</a></li>
@@ -186,19 +186,19 @@
 	<?php // Homepage Layout 2 Tile 5 (Twitter Feed Tile) ?>
 	<?php $hp2_tile_5_bg_image = get_field('twitter_feed_tile_bg_image'); ?>
 	<?php if( !empty($hp2_tile_5_bg_image) ): ?>
-		<div class="threecol grid grayscale" id="twitter_feed_tile" style="background-image: url(<?php echo $hp2_tile_5_bg_image['url']; ?>);">Twitter Carousel</div>
+		<div class="threecol grid gridbg grayscale" id="twitter_feed_tile" style="background-image: url(<?php echo $hp2_tile_5_bg_image['url']; ?>);">Twitter Carousel</div>
 	<?php endif; ?>
 
 	<?php // Homepage Layout 2 Tile 6 (Lookbook Tile) ?>
 	<?php $hp2_tile_6_bg_image = get_field('lookbook_tile_bg_image'); ?>
 	<?php if( !empty($hp2_tile_6_bg_image) ): ?>
-		<div class="threecol grid clearfix" id="tile_6"><a href="/lookbook"><img class="grayscale" src="<?php echo $hp2_tile_6_bg_image['url']; ?>" alt="<?php echo $hp2_tile_6_bg_image['alt']; ?>" /></a></div>
+		<div class="threecol grid gridbg clearfix" id="tile_6" style="background-image: url(<?php echo $hp2_tile_6_bg_image['url']; ?>);"><a href="/lookbook"><h2>2014 <strong>Lookbook</strong></h2></a></div>
 	<?php endif; ?>
 
 	<?php // Homepage Layout 2 Tile 7 (Follow Us Tile) ?>
 	<?php $hp2_tile_7_bg_image = get_field('follow_us_tile_hp2_bg_image'); ?>
 	<?php if( !empty($hp2_tile_7_bg_image) ): ?>
-		<div class="sixcol grid grayscale clearfix hideme" id="follow_us_tile" style="background-image: url(<?php echo $hp2_tile_7_bg_image['url']; ?>);">
+		<div class="sixcol grid gridbg grayscale clearfix hideme" id="follow_us_tile" style="background-image: url(<?php echo $hp2_tile_7_bg_image['url']; ?>);">
 			<h2>Follow Us</h2>
 			<ul class="clearfix">
 				<li class="facebook"><a class="image-replacement" href="https://www.facebook.com/edhardy" target="_blank">Facebook (https://www.facebook.com/edhardy)</a></li>
@@ -212,7 +212,7 @@
 	<?php // Homepage Layout 2 Tile 8 (Latest News Tile) ?>
 	<?php $hp2_tile_8_bg_image = get_field('latest_news_tile_bg_image'); ?>
 	<?php if( !empty($hp2_tile_8_bg_image) ): ?>
-		<div class="threecol grid clearfix" id="tile_8"><a href="#newsletter"><img class="grayscale" src="<?php echo $hp2_tile_8_bg_image['url']; ?>" alt="<?php echo $hp2_tile_8_bg_image['alt']; ?>" /></a></div>
+		<div class="threecol grid gridbg clearfix" id="tile_8" style="background-image: url(<?php echo $hp2_tile_8_bg_image['url']; ?>);"><a href="#newsletter"><h2>Latest <strong>News</strong></h2></a></div>
 	<?php endif; ?>
 
 <?php endif; ?>
@@ -230,19 +230,19 @@
 	<?php // Homepage Layout 3 Tile 2 (Lookbook Tile) ?>
 	<?php $hp3_tile_2_bg_image = get_field('lookbook_tile_bg_image'); ?>
 	<?php if( !empty($hp3_tile_2_bg_image) ): ?>
-		<div class="threecol grid clearfix" id="tile_2"><a href="/lookbook"><img class="grayscale" src="<?php echo $hp3_tile_2_bg_image['url']; ?>" alt="<?php echo $hp3_tile_2_bg_image['alt']; ?>" /></a></div>
+		<div class="threecol grid gridbg clearfix" id="tile_2" style="background-image: url(<?php echo $hp3_tile_2_bg_image['url']; ?>);"><a href="/lookbook"><h2>2014 <strong>Lookbook</strong></h2></a></div>
 	<?php endif; ?>
 
 	<?php // Homepage Layout 3 Tile 3 (Twitter Feed Tile) ?>
 	<?php $hp3_tile_3_bg_image = get_field('twitter_feed_tile_bg_image'); ?>
 	<?php if( !empty($hp3_tile_3_bg_image) ): ?>
-		<div class="threecol grid grayscale" id="twitter_feed_tile" style="background-image: url(<?php echo $hp3_tile_3_bg_image['url']; ?>);">Twitter Carousel</div>
+		<div class="threecol grid gridbg grayscale" id="twitter_feed_tile" style="background-image: url(<?php echo $hp3_tile_3_bg_image['url']; ?>);">Twitter Carousel</div>
 	<?php endif; ?>
 
 	<?php // Homepage Layout 3 Tile 4 (Latest News Tile) ?>
 	<?php $hp3_tile_4_bg_image = get_field('latest_news_tile_bg_image'); ?>
 	<?php if( !empty($hp3_tile_4_bg_image) ): ?>
-		<div class="threecol grid clearfix" id="tile_4"><a href="#newsletter"><img class="grayscale" src="<?php echo $hp3_tile_4_bg_image['url']; ?>" alt="<?php echo $hp3_tile_4_bg_image['alt']; ?>" /></a></div>
+		<div class="threecol grid gridbg clearfix" id="tile_4" style="background-image: url(<?php echo $hp3_tile_4_bg_image['url']; ?>);"><a href="#newsletter"><h2>Latest <strong>News</strong></h2></a></div>
 	<?php endif; ?>
 
 	<?php // Homepage Layout 3 Tile 5 (Instagram Tile) ?>
