19b65754756c2549329986f65115c208e4bf5fbb
Agustin Feliz <agustin.feliz@gmail.com>
2014-03-25 18:05:13 -0400

    Video responsive

--- footer-video.php
+++ footer-video.php
@@ -9,15 +9,17 @@
 
 	<div class="twelvecol grid clearfix" id="footer_video">
 
-		<video width="1200" height="680" id="player1" poster="<?php echo $footer_video_poster_image['url']; ?>">
+		<video width="1200" height="680" style="width: 100%; height: 100%;" id="player1" poster="<?php echo $footer_video_poster_image['url']; ?>">
 		    <source type="video/youtube" src="<?php echo $footer_video_youtube_url; ?>" />
 		</video>
-		
+
 		<script>
-		jQuery('video').mediaelementplayer({});
+		jQuery('video').mediaelementplayer({
+			startVolume: 0.5, // initial volume when the player starts
+			features: ['playpause','progress','tracks','volume','fullscreen']
+		});
 		</script>
 
 	</div>
 
 <?php endif; ?>
-
