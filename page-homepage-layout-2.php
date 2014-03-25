df68f8c65173d2891aaa755be925f13642e4441a
Agustin Feliz <agustin.feliz@gmail.com>
2014-03-25 01:31:18 -0400

    American Art Legend include & css tweak

--- page-homepage-layout-2.php
+++ page-homepage-layout-2.php
@@ -18,13 +18,7 @@ Template Name: Homepage Layout 2
 					<h2 class="image-replacement">Who Is Ed Hardy?</h2>
 				</div>
 
-				<div class="twelvecol clearfix" id="american-art-legend">
-					<h2 class="image-replacement">An American Art Legend</h2>
-					<p>Don Ed Hardy brought tattoo artistry out of the dark and into the glamorous world of fashion. He quickly became recognized for his incorporation of the Japanese art form and American style. In 2002, Hardy stepped into the fashion space when he licensed his art designs to be made into a clothing line. In two short years the Ed Hardy line became one of the most desired and recognizable brands of its time. Don Ed Hardy continues to be one of the most influential designers and his artwork has stood the test of time in the tattoo, art, and fashion world.</p>
-				</div>
-
-				<!-- <div id="fb-root"></div> <script>(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//connect.facebook.net/en_US/all.js#xfbml=1"; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk'));</script>
-				<div class="fb-post" data-href="https://www.facebook.com/photo.php?v=1410782879144911" data-width="466"><div class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/photo.php?v=1410782879144911">Post</a> by <a href="https://www.facebook.com/edhardy">ED HARDY</a>.</div></div> -->
+				<?php include('american-art-legend.php'); ?>
 
 			</div>
 
