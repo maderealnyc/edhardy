8501969fb6eaea9e403fac69fc109516e66a0cb2
Agustin Feliz <agustin.feliz@gmail.com>
2014-03-27 02:00:17 -0400

    Parallax tweaks #image_below_grid now background-image based to fix Chrome jitterness mobile/tablet tweaks

--- homepage-variables-controller.php
+++ homepage-variables-controller.php
@@ -262,5 +262,5 @@
 
 <?php $image_below_grid = get_field('image_below_grid'); ?>
 <?php if( !empty($image_below_grid) ): ?>
-	<div class="twelvecol clearfix" id="image_below_grid"><img src="<?php echo $image_below_grid['url']; ?>" alt="<?php echo $image_below_grid['alt']; ?>" data-stellar-ratio="0.75" /></div>
+	<div class="twelvecol clearfix" id="image_below_grid" style="background-image: url(<?php echo $image_below_grid['url']; ?>);" data-stellar-background-ratio="0.75"></div>
 <?php endif; ?>
