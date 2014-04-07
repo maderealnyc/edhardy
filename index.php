32e2af168235be7b7acb6a1019328d27e896e364
Gennaro Monaco <gennaromonaco@yahoo.com>
2014-04-07 17:15:46 -0400

    One index.php to rule them all

--- index.php
+++ index.php
@@ -6,63 +6,110 @@
 
 						<div id="main" class="eightcol first clearfix" role="main">
 
+							<?php if (is_category()) { ?>
+								<h1 class="archive-title h2"><span><?php _e( 'Category:', 'bonestheme' ); ?></span> <?php single_cat_title(); ?></h1>
+
+							<?php } elseif (is_tag()) { ?>
+								<h1 class="archive-title h2"><span><?php _e( 'Tag:', 'bonestheme' ); ?></span> <?php single_tag_title(); ?></h1>
+
+							<?php } elseif (is_author()) {
+								global $post;
+								$author_id = $post->post_author;
+							?>
+								<h1 class="archive-title h2"><span><?php _e( 'Posts By:', 'bonestheme' ); ?></span> <?php the_author_meta('display_name', $author_id); ?></h1>
+
+							<?php } elseif (is_day()) { ?>
+								<h1 class="archive-title h2"><span><?php _e( 'Daily Archives:', 'bonestheme' ); ?></span> <?php the_time('l, F j, Y'); ?></h1>
+
+							<?php } elseif (is_month()) { ?>
+								<h1 class="archive-title h2"><span><?php _e( 'Monthly Archives:', 'bonestheme' ); ?></span> <?php the_time('F Y'); ?></h1>
+
+							<?php } elseif (is_year()) { ?>
+								<h1 class="archive-title h2"><span><?php _e( 'Yearly Archives:', 'bonestheme' ); ?></span> <?php the_time('Y'); ?></h1>
+
+							<?php } elseif (is_search()) { ?>
+								<h1 class="archive-title h2"><span><?php _e( 'Search Results for:', 'bonestheme' ); ?></span> <?php echo esc_attr(get_search_query()); ?></h1>
+
+							<?php } ?>
+
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article">
+								<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article">
 
-								<header class="article-header">
+									<header class="article-header">
 
-									<h1 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-									<!--<p class="byline vcard"><?php
-										//printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), bones_get_the_author_posts_link(), get_the_category_list(', '));
-									?></p>-->
+										<h1 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 
-								</header>
+										<!--<p class="byline vcard"><?php
+											//printf(__( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(__( 'F jS, Y', 'bonestheme' )), bones_get_the_author_posts_link(), get_the_category_list(', '));
+										?></p> -->
 
-								<section class="entry-content clearfix">
-									<?php if ( has_post_thumbnail() ) : ?> 
-										<a title="<?php the_title_attribute(); ?>" href="<?php the_permalink(); ?>">   
-											<?php the_post_thumbnail(full); ?>      
-								   		</a> 
-									<?php endif; ?> 
+									</header>
 
-									<?php the_excerpt('Read More...'); ?>
-								</section>
+									<section class="entry-content clearfix">
 
-								<footer class="article-footer">
-									<p class="tags"><?php the_tags( '<span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '' ); ?></p>
+										<?php if ( has_post_thumbnail() ) : ?>
+											<a title="<?php the_title_attribute(); ?>" href="<?php the_permalink(); ?>"><?php the_post_thumbnail(full); ?></a>
+										<?php endif; ?> 
 
-								</footer>
+										<?php echo get_the_excerpt(); ?> <a href="<?php the_permalink() ?>">Read More...</a>
 
-								<?php // comments_template(); // uncomment if you want to use them ?>
+									</section>
 
-							</article>
+									<footer class="article-footer">
+
+										<!--<p class="tags"><?php //the_tags( '<span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '' ); ?></p>-->
+
+									</footer>
+
+									<?php // comments_template(); // uncomment if you want to use them ?>
+
+								</article>
 
 							<?php endwhile; ?>
 
-									<?php if ( function_exists( 'bones_page_navi' ) ) { ?>
-											<?php bones_page_navi(); ?>
-									<?php } else { ?>
-											<nav class="wp-prev-next">
-													<ul class="clearfix">
-														<li class="prev-link"><?php next_posts_link( __( '&laquo; Older Entries', 'bonestheme' )) ?></li>
-														<li class="next-link"><?php previous_posts_link( __( 'Newer Entries &raquo;', 'bonestheme' )) ?></li>
-													</ul>
-											</nav>
-									<?php } ?>
+							<?php if ( function_exists( 'bones_page_navi' ) ) { ?>
+
+								<?php bones_page_navi(); ?>
+
+								<?php } else { ?>
+
+										<nav class="wp-prev-next">
+
+												<ul class="clearfix">
+
+													<li class="prev-link"><?php next_posts_link( __( '&laquo; Older Entries', 'bonestheme' )) ?></li>
+
+													<li class="next-link"><?php previous_posts_link( __( 'Newer Entries &raquo;', 'bonestheme' )) ?></li>
+
+												</ul>
+
+										</nav>
+
+								<?php } ?>
 
 							<?php else : ?>
 
 									<article id="post-not-found" class="hentry clearfix">
-											<header class="article-header">
-												<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+
+										<header class="article-header">
+
+											<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+
 										</header>
-											<section class="entry-content">
-												<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+
+										<section class="entry-content">
+
+											<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+
 										</section>
+
 										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the index.php template.', 'bonestheme' ); ?></p>
+
+											<p><?php _e( 'This is the error message in the index.php template.', 'bonestheme' ); ?></p>
+
 										</footer>
+
 									</article>
 
 							<?php endif; ?>
