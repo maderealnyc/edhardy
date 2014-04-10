5583d0f7bf4c69c605fe16520760d712d26cffdf
Gennaro Monaco <gennaromonaco@yahoo.com>
2014-04-10 12:10:45 -0400

    Early work on Connect Template (Phase 3)

--- page-connect.php
+++ page-connect.php
@@ -5,40 +5,36 @@ Template Name: Connect Template
 ?>
 
 <?php get_header(); ?>
+<?php $connect_instagram_hashtag = get_field('connect_instagram_hashtag'); ?>
 
-<div id="content">
+	<div id="content">
 
-	<div id="inner-content" class="wrap clearfix">
+		<div id="inner-content" class="wrap clearfix">
 
-		<div id="main" class="eightcol first clearfix" role="main">
+			<div id="main" class="twelvecol first clearfix" role="main">
 
-			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<p><img src="<?php echo get_template_directory_uri(); ?>/library/images/star.png"/>CONNECT<img src="<?php echo get_template_directory_uri(); ?>/library/images/star.png"/></p>
+				<p><?php echo '#' . $connect_instagram_hashtag; ?></p>
+				<p>Hashtag your Instagram photos <strong><?php echo '#' . $connect_instagram_hashtag; ?></strong> and we’ll add you to our gallery!</p>
+				<p>Follow us at <a href="http://instagram.com/OfficialEdHardy" target="_blank">instagram.com/OfficialEdHardy</a>.</p>
 
-			<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+				<script type="text/javascript">
+				    var connectFeed = new Instafeed({
+				        get: 'tagged',
+				        tagName: <?php echo "'" . $connect_instagram_hashtag . "'"; ?>,
+				        clientId: 'f3b5503d3d114e28991618cc5e64815d',
+				        target: 'connect_tiles',
+				        resolution: 'low_resolution',
+				        template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /></a>'
+				    });
+				    connectFeed.run();
+				</script>
+				<div id="connect_tiles"></div>
 
-				<section class="entry-content clearfix" itemprop="articleBody">
-					<?php the_content(); ?>
-				</section>
-			</article>
-
-			<?php endwhile; else : ?>
-
-					<article id="post-not-found" class="hentry clearfix">
-						<header class="article-header">
-							<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-						</header>
-						<section class="entry-content">
-							<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-						</section>
-						<footer class="article-footer">
-								<p><?php _e( 'This is the error message in the page.php template.', 'bonestheme' ); ?></p>
-						</footer>
-					</article>
-
-			<?php endif; ?>
+			</div>
 
 		</div>
+
 	</div>
-</div>
 
 <?php get_footer(); ?>
